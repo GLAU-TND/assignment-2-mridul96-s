eb66ae0a718493ebf5764d652c131bf61d42395a
Mridul srivastava <62951259+mridul96-s@users.noreply.github.com>
2020-03-31 22:55:00 +0530

    Update MyBinarySearchTree.java
    final commit

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -4,7 +4,31 @@
  *  Date: 23-Mar-20
  *  Time: 7:17 PM
  */
-package problem1.mybst;
-// to implement BinarySearchTree
 public class MyBinarySearchTree {
-}
+    TreeNode root;
+    int mlevel = 0, c = 0;
+
+    public MyBinarySearchTree() {
+        this.root = root;
+    }
+
+    public void insert(int data) {
+        TreeNode newnode = new TreeNode(data);
+        if (root == null) {
+            root = newnode;
+        } else {
+            TreeNode temp = root;
+            TreeNode parent = null;
+            while (temp != null) {
+                parent = temp;
+                if (newnode.getData() >= temp.getData())
+                    temp = temp.getRight();
+                else
+                    temp = temp.getLeft();
+            }
+            if (newnode.getData() <= parent.getData())
+                parent.setLeft(newnode);
+            else
+                parent.setRight(newnode);
+        }
+    }
